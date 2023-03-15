6e7085d08de733aaad6a10faf9a5b7ccec1cec58
Rock-Bautumn <3623@holbertonschool.com>
2023-03-15 16:02:11 -0500

    Trying a new interval and swap strategy for shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -2,38 +2,58 @@
 #include "sort.h"
 
 /**
+ * swap_array_items - Swap the items of an int array
+ * @array: The int array that contains the items to swap
+ * @item1: The first item of the array to swap
+ * @item2: The second item of the array to swap
+ * Return: void
+*/
+void swap_array_items(int *array, size_t item1, size_t item2)
+{
+	int temp;
+
+	if (array == NULL)
+		return;
+
+	temp = array[item1];
+	array[item1] = array[item2];
+	array[item2] = temp;
+}
+
+/**
  * shell_sort - Sort an int array using the Knuth sequence
  * @array: The array to sort
  * @size: The size of the array
  * Return: void
 */
+
 void shell_sort(int *array, size_t size)
 {
-	int gap = 1, left, right, this_item;
+	size_t gap = 1, outer, inner;
 
-	/* Find the initial gap size */
-	while (gap <= (int) (size - 1) / 9)
-		gap = 3 * gap + 1;
+	/* check if we can sort the array */
+	if (array == NULL || size < 2)
+		return;
 
-	/* Iterate through the array with varying gaps */
-	for (; gap >= 1; gap = gap / 3)
+	/* Find our initial gap */
+
+	while (gap <= size)
+		gap = gap * 3 + 1;
+	gap = (gap - 1) / 3;
+
+	for (; gap > 0; gap = (gap - 1) / 3)
 	{
-		for (right = gap; right < (int) size; right++)
+		for (outer = 0; outer < size - gap; outer++)
 		{
-			this_item = array[right];
-			/*
-			 * This for loop requires multiple lines to comply
-			 * with Betty style requirements due to meaningful
-			 * variable names
-			*/
-			for (left = right - gap;
-			left >= 0 && this_item < array[left];
-			left = left - gap)
-				array[left + gap] = array[left];
-			array[left + gap] = this_item;
-		}
+			for (inner = outer + gap; inner >= gap; inner--)
+			{
+				if (array[inner - gap] > array[inner])
+					swap_array_items(array, inner, inner - gap);
 
+				if (inner == 0)
+					break;
+			}
+		}
 		print_array(array, size);
 	}
-
 }
